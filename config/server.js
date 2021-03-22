4006c462523df73cebdada3662c6c6921bc9c68b
Matheus Riguette <matheus.f.riguette@gmail.com>
2021-03-22 01:41:42 -0300

    Chore: First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '07d2e5eb131096064f4b6e09eb0381cc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cba7b9fe-2c82-4007-912d-24a3b1d193ec'
+};
